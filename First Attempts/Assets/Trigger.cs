4a03aa855274e459fddc9dd1d0bc4bd63c3cb0f7
Cameron burridge <cmburridge4022@gmail.com>
2019-09-03 21:24:23 -0600

    Attempted to recreate in class process

---
+++ First Attempts/Assets/Trigger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		throw new NotImplementedException();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
