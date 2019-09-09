bbe6baf1625dfd881fc9e36c936b9005c9ad2eb3
Cameron burridge <cmburridge4022@gmail.com>
2019-09-08 20:58:55 -0600

    Solved Ontrigger error

---
+++ First Attempts/Assets/Behaviors.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behaviors : MonoBehaviour
+{
+
+	public float floatValue = 5f;
+
+	public int intValue = 20;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- First Attempts/Assets/Trigger.cs
+++ First Attempts/Assets/Trigger.cs
@@ -6,17 +6,20 @@ using UnityEngine.Events;
 
 public class Trigger : MonoBehaviour
 {
+	
 	public float floatValue = 5f;
+	public int intValue = 20;
 	public string stringValue = "Jon";
 	public UnityEvent Event;
+	
+
 	public void OnTriggerEnter(Collider other)
 	{
-		Event.Invoke();
+		throw new NotImplementedException();
 	}
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
