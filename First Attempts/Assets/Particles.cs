dbd9544d94661cbe2ab6bf0441aa60dfa470a562
Cameron burridge <cmburridge4022@gmail.com>
2019-09-13 22:14:55 -0600

    Created particle system on mouse down

---
+++ First Attempts/Assets/Particles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Particles : MonoBehaviour
+{
+	public UnityEvent emitParticles;
+
+	private void OnMouseDown()
+	{
+		emitParticles.Invoke();
+	}
+}
