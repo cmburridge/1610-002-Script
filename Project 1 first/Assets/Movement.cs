a29d80fac7b7a583bf969eefada4bb3d298dcbce
Cameron Burridge <cmburridge4022@gmail.com>
2019-09-17 15:47:42 -0600

    Created Moving character Controller in class

---
+++ Project 1 first/Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float gravity = 3f;
+    private Vector3 position;
+    public float jumpSpeed = -10f;
+    public CharacterController controller;
+    // Update is called once per frame
+    void Update()
+    {
+        position.x = speed * Input.GetAxis("Horizontal");
+        if (controller.isGrounded)
+        {
+            position.y -= gravity;
+        }
+       
+        controller.Move(position * Time.deltaTime);
+        position.y -= gravity;
+        if (Input.GetButtonDown("Jump"))
+        {
+            position.y = jumpSpeed;
+        }
+    }
+}
