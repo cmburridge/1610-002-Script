698e22c3e337b12b0b47aec6e756380c98e0c443
Cameron burridge <cmburridge4022@gmail.com>
2019-09-18 21:47:57 -0600

    Recreated the movement script from class at home

---
+++ First Attempts/Assets/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public CharacterController controller;
+	public float speed = 5f;
+	public float gravity = 3f;
+	public Vector3 position;
+	public float jumpSpeed = 4f;
+	// Update is called once per frame
+	void Update ()
+	{
+		position.x = speed * Input.GetAxis("Horizontal");
+		if (controller.isGrounded)
+		{
+			position.y = gravity;
+		}
+		controller.Move(position * Time.deltaTime);
+		position.y -= gravity;
+		if (Input.GetButtonDown("Jump"))
+		{
+			position.y = jumpSpeed;
+		}
+	}
+}
