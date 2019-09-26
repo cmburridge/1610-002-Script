1f5349feaae7df0eb1e83e06fb0ab53e48175ad7
Cameron Burridge <cmburridge4022@gmail.com>
2019-09-26 15:47:35 -0600

    Create Functions and Character data

---
+++ Project 1 first/Assets/Functions.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+
+public class Functions : MonoBehaviour
+{
+    public string playerName;
+    private int myNumber;
+    public float speed = 5;
+    public GameObject player;
+    public Color playerColor = Color.red;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConfigGameObject();
+    }
+
+    
+    
+
+    private GameObject ConfigGameObject()
+    {
+        var newPlayer = Instantiate(player);
+        newPlayer.layer = 0;
+        newPlayer.active = true;
+        newPlayer.tag = "player";
+        newPlayer.transform.position = Vector3.zero;
+        newPlayer.name = playerName;
+        newPlayer.GetComponent<Renderer>().material.color = playerColor;
+        
+        return newPlayer;
+    }
+    
+    
+    private void OnTriggerEnter(Collider other)
+    {
+      speed = IncreaseSpeed(2f);
+    }
+    
+    
+    private float IncreaseSpeed (float multiplier)
+    {
+        return speed * multiplier;
+    }
+    
+}
