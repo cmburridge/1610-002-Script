beb4c2e9f2590606ac0476210b7c0f849e67fa6e
Cameron burridge <cmburridge4022@gmail.com>
2019-09-25 22:50:20 -0600

    Made rubber material for rubber object

---
+++ First Attempts/Assets/Rubber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class Rubber : MonoBehaviour
+{
+	public Vector3 bouncy;
+	private Rigidbody rb;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		rb.AddForce(bouncy);
+	}
+}
