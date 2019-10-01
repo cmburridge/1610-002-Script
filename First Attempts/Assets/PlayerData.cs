7f3e9748d8a3ae8d6fd7e333b71d2b5800775525
Cameron burridge <cmburridge4022@gmail.com>
2019-09-30 23:31:55 -0600

    Created Player Data

---
+++ First Attempts/Assets/PlayerData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+    public float speed = 1f;
+    public float height = 1f;
+    public float weight = 1f;
+    public float jumpSpeed = 1f;
+    public float strength = 1f;
+    public float defense = 1f;
+    public float magic = 1f;
+    public float charisma = 1f;
+    public bool backFlip = true;
+    public bool cold = true;
+    public bool canJump = true;
+    public bool canSwim = true;
+    public bool weaponEquipped = true;
+    public bool canDie = true;
+    public bool hidden = true;
+
+
+}
