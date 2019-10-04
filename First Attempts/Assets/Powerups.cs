954c60bf55c58e2fcc91d060b2bf1b6c72cdf6f4
Cameron burridge <cmburridge4022@gmail.com>
2019-10-03 23:31:28 -0600

    Recreating process done in class, couldn't make health bar go down, but enemy does take down health.

---
+++ First Attempts/Assets/Powerups.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Powerups : ScriptableObject
+{
+
+	public float HealthAmount = 1f;
+	public float minValue = 0;
+	public float maxValue = 1f;
+	
+	
+	public void UpdateValue(float amount)
+	{
+		HealthAmount += amount;
+	}
+
+	public void UpdateValueRange(float amount)
+	{
+		if (HealthAmount <= maxValue)
+		{
+			UpdateValue(amount);
+		}
+
+		if (HealthAmount >= minValue)
+		{
+			UpdateValue(amount);
+		}
+		else
+		{
+			HealthAmount = minValue;
+		}
+		
+		
+	}
+	
+	
+}
---
+++ First Attempts/Assets/UiImageController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class UiImageController : MonoBehaviour
+{
+
+	private Image img;
+	// Use this for initialization
+	void Awake ()
+	{
+		img = GetComponent<Image>();
+	}
+
+	public void UpdateImage(Powerups data)
+	{
+		img.fillAmount = data.HealthAmount;
+	}
+
+}
