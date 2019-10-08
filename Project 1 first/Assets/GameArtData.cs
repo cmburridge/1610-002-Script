ccd4a06d92d1eb87aa4aa114ebdc27444d1ec0b8
Cameron Burridge <cmburridge4022@gmail.com>
2019-10-08 15:57:02 -0600

    In class, player data and instantiating players.

---
+++ Project 1 first/Assets/GameArtData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameArtData : ScriptableObject
+{
+    public Sprite sprite;
+    public Color color;
+    public GameObject prefab;
+}
