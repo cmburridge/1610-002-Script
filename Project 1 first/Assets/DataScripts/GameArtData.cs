d090216005e941a0e65e5e2a2c1a899536c78fcf
Cameron Burridge <cmburridge4022@gmail.com>
2019-10-22 15:49:24 -0600

    Added Collectibles in class

--- Project 1 first/Assets/DataScripts/GameArtData.cs
+++ Project 1 first/Assets/DataScripts/GameArtData.cs
@@ -7,4 +7,5 @@ public class GameArtData : ScriptableObject
     public Sprite sprite;
     public Color color = Color.red;
     public GameObject prefab;
+    public bool collected;
 }
--- Project 1 first/Assets/EnemyOne1.cs
+++ Project 1 first/Assets/EnemyOne1.cs
@@ -1,25 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu]
-public class EnemyOne1 : GameArtData
+public class EnemyOne1 : MonoBehaviour
 {
-    public Transform placement;
+   
     public Vector3 move;
     public Rigidbody enemy;
 
     void Update()
     {
-        placement.Translate(move*Time.deltaTime);
+       transform.Translate(move*Time.deltaTime);
         enemy.MovePosition(move*Time.deltaTime);
     }
-    public void instanceEnemy()
-    {
-        var enemy1 = Instantiate(prefab);
-        var enemySprite = enemy1.GetComponentInChildren<SpriteRenderer>();
-        enemySprite.sprite = sprite;
-        enemySprite.color = color;
+
  
     }
-}
-
