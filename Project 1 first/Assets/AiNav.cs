3911fd509c525929f9bcce6057c87e0b806b5861
Cameron Burridge <cmburridge4022@gmail.com>
2019-11-05 15:53:41 -0700

    Created AI following mouse and mouse down. inClass

---
+++ Project 1 first/Assets/AiNav.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AiNav : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Vector3Data location;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = location.value;
+    }
+}
---
+++ Project 1 first/Assets/MouseEvents1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class MouseEvents1 : MonoBehaviour
+{
+    private Camera cam;
+    public Vector3Data data;
+    
+    
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void OnMouseDown()
+    {
+        var ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit))
+        {
+            data.value = hit.point;
+        }
+    }
+}
