3b14fe4f09f64aba5943c7eb2ee319f38a2e17f8
Cameron Burridge <cmburridge4022@gmail.com>
2019-11-12 15:48:37 -0700

    Coeruetines

---
+++ Project 1 first/Assets/Scenes/CodeRutiens.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CodeRutiens : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public float seconds = 10f; 
+    public int counter = 10;
+    public bool canRun;
+    private WaitForSeconds wfsObj;
+
+    private void Awake()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+    }
+
+    public void callCodeRuetine()
+    {
+        StartCoroutine(RunCodeRuetine());
+    }
+
+    IEnumerator RunCodeRuetine()
+    {
+        startEvent.Invoke();
+        while (counter > 0)
+        {
+           repeatEvent.Invoke();
+           yield return wfsObj;
+            counter--;
+        }
+        while (canRun)
+        {
+            repeatEvent.Invoke();
+            yield return wfsObj;
+            counter--;
+        }
+        endEvent.Invoke();
+    }
+}
