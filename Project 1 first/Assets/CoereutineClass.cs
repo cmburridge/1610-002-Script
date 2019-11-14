f38f93ffa6ed9fc9c3e4dfe673b34794b7ed8609
Cameron Burridge <cmburridge4022@gmail.com>
2019-11-14 15:44:41 -0700

    Recreated the coeruetine script. Inclass

---
+++ Project 1 first/Assets/CoereutineClass.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoereutineClass : MonoBehaviour
+{
+    public bool canRun = true;
+    public IntData index;
+    public float seconds = 1f;
+    private WaitForSeconds wfsobj;
+    
+    public void Run()
+    {
+        wfsobj = new WaitForSeconds(seconds);
+        StartCoroutine(OnRun());
+    }
+
+    IEnumerator OnRun()
+    {
+        while (index.value > 0)
+        {
+             Debug.Log(index.value);
+             index.value--;
+             yield return wfsobj;
+        }
+    }
+}
